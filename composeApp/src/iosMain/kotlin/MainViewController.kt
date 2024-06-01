5247986a61bba23fe59db108cec7b8ad5d9ba31a
Akexorcist <akexorcist@gmail.com>
2024-06-01 14:24:54 +0700

    Enable screen navigation with hardware keyboard on iOS

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -3,9 +3,84 @@ import com.akexorcist.kotlin.multiplatform.ui.content.App
 import com.akexorcist.kotlin.multiplatform.ui.navigation.rememberScreenNavigationState
 
 @Suppress("FunctionName")
-fun MainViewController() = ComposeUIViewController {
+fun MainViewController(navigation: PlatformKeyboardNavigation) = ComposeUIViewController {
     val screenNavigationState = rememberScreenNavigationState()
+    navigation.addOnKeyEventListener(
+        PlatformKeyboardNavigationAdapter(
+            onLeftPressed = { screenNavigationState.back() },
+            onMetaLeftPressed = { screenNavigationState.first() },
+            onRightPressed = { screenNavigationState.next() },
+            onMetaRightPressed = { screenNavigationState.last() },
+            onSpacePressed = { screenNavigationState.next() },
+        )
+    )
     App(
         screenNavigationState = screenNavigationState,
     )
+}
+
+class PlatformKeyboardNavigation {
+    private val listeners = arrayListOf<Listener>()
+
+    fun onLeftPressed() {
+        listeners.forEach { it.onLeftPressed() }
+    }
+
+    fun onMetaLeftPressed() {
+        listeners.forEach { it.onMetaLeftPressed() }
+    }
+
+    fun onRightPressed() {
+        listeners.forEach { it.onRightPressed() }
+    }
+
+    fun onMetaRightPressed() {
+        listeners.forEach { it.onMetaRightPressed() }
+    }
+
+    fun onSpacePressed() {
+        listeners.forEach { it.onSpacePressed() }
+    }
+
+    fun addOnKeyEventListener(
+        listener: Listener
+    ) {
+        listeners.add(listener)
+    }
+
+    interface Listener {
+        fun onLeftPressed()
+        fun onMetaLeftPressed()
+        fun onRightPressed()
+        fun onMetaRightPressed()
+        fun onSpacePressed()
+    }
+}
+
+private class PlatformKeyboardNavigationAdapter(
+    onLeftPressed: () -> Unit,
+    onMetaLeftPressed: () -> Unit,
+    onRightPressed: () -> Unit,
+    onMetaRightPressed: () -> Unit,
+    onSpacePressed: () -> Unit,
+) : PlatformKeyboardNavigation.Listener {
+    override fun onLeftPressed() {
+        onLeftPressed()
+    }
+
+    override fun onMetaLeftPressed() {
+        onMetaLeftPressed()
+    }
+
+    override fun onRightPressed() {
+        onRightPressed()
+    }
+
+    override fun onMetaRightPressed() {
+        onMetaRightPressed()
+    }
+
+    override fun onSpacePressed() {
+        onSpacePressed()
+    }
 }
--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -3,19 +3,40 @@ import SwiftUI
 import ComposeApp
 
 struct ComposeView: UIViewControllerRepresentable {
+    private let navigation: PlatformKeyboardNavigation
+    
+    init(navigation: PlatformKeyboardNavigation) {
+        self.navigation = navigation
+    }
+    
     func makeUIViewController(context: Context) -> UIViewController {
-        MainViewControllerKt.MainViewController()
+        MainViewControllerKt.MainViewController(navigation: navigation)
     }
 
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
 }
 
 struct ContentView: View {
+    private let navigation = PlatformKeyboardNavigation()
+    
     var body: some View {
-        ComposeView()
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+        ComposeView(navigation: navigation)
+            .onKeyPress(.leftArrow) {
+                navigation.onLeftPressed()
+                return .handled
+            }
+            .onKeyPress(.rightArrow) {
+                navigation.onRightPressed()
+                return .handled
+            }
+            .onKeyPress(.rightArrow) {
+                navigation.onSpacePressed()
+                return .handled
+            }
+            .onKeyPress(.space) {
+                navigation.onSpacePressed()
+                return .handled
+            }
+            .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
-
-
-
