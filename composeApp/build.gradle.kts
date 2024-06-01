6d2510707726cc04277bc49a6bf0d426b9b1da0e
Akexorcist <akexorcist@gmail.com>
2024-06-01 17:35:43 +0700

    Use Kotlin 2.0

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.kotlinComposePlugin)
     id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
