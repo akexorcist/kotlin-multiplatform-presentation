b680cc67699ecd1addf64816d9d85f8ea2adaaac
Akexorcist <akexorcist@gmail.com>
2024-06-21 11:07:54 +0700

    Add compose multiplatform limitation content

--- composeApp/src/commonMain/kotlin/com/akexorcist/kotlin/multiplatform/ui/content/compose/ComposeMultiplatformLimitationScreen.kt
+++ composeApp/src/commonMain/kotlin/com/akexorcist/kotlin/multiplatform/ui/content/compose/ComposeMultiplatformLimitationScreen.kt
@@ -1,10 +1,16 @@
 package com.akexorcist.kotlin.multiplatform.ui.content.compose
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.akexorcist.kotlin.multiplatform.ui.component.ContentText
+import com.akexorcist.kotlin.multiplatform.ui.component.template.DefaultCustomContentTemplate
 import com.akexorcist.kotlin.multiplatform.ui.component.template.DefaultTemplate
 import com.akexorcist.kotlin.multiplatform.ui.content.ComposeTag
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -24,11 +30,19 @@ fun NavGraphBuilder.composeMultiplatformLimitation() {
 
 @Composable
 fun ComposeMultiplatformLimitationScreen() {
-    DefaultTemplate(
-        title = "Compose Multiplatform Limitation",
-        content = "",
+    DefaultCustomContentTemplate(
+        title = "Limitation",
         tag = ComposeTag,
-    )
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+        ContentText(text = "• Platform UI Interoperability")
+        Spacer(modifier = Modifier.height(16.dp))
+        ContentText(text = "• UI Rendering Performance")
+        Spacer(modifier = Modifier.height(16.dp))
+        ContentText(text = "• UI Inspection Tools")
+        Spacer(modifier = Modifier.height(16.dp))
+        ContentText(text = "• WebAssembly requires for Web")
+    }
 }
 
 @Preview
